efd3c3c4662e444a227046ef234cc8e7681ffbbb
Abderrazzaq Khastaf <akhastaf@e0r3p2.1337.ma>
2021-09-25 13:28:18 +0100

    finish the ex02 on CPP06

--- CPP06/ex02/Base.hpp
+++ CPP06/ex02/Base.hpp
@@ -1,8 +1,10 @@
 #ifndef BASE_HPP
 # define BASE_HPP
 # include <iostream>
+# include <time.h>
 
 class Base {
+    public:
 	virtual ~Base(){};
 };
 
--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -1,28 +1,83 @@
-#include <iostream>
+#include "Base.hpp"
 
-struct  Data {
-    std::string name;
-    unsigned int age;
-};
-
-uintptr_t serialize(Data* ptr)
+Base * generate(void)
 {
-    return reinterpret_cast<uintptr_t>(ptr);
+    int r = rand() % 3;
+    if (!r)
+        return new A();
+    else if (r == 1)
+        return new B();
+    return new C();
 }
 
-Data* deserialize(uintptr_t raw)
+void identify(Base * p)
 {
-    return reinterpret_cast<Data *>(raw);
+
+    if ((dynamic_cast<A*>(p)))
+        std::cout << "A" << std::endl;
+    else if ((dynamic_cast<B*>(p)))
+        std::cout << "B" << std::endl;
+    else if ((dynamic_cast<C*>(p)))
+        std::cout << "C" << std::endl;
+}
+
+void identify( Base & p)
+{
+    Base tmp;
+    try
+    {
+        tmp = dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch(const std::bad_cast & e)
+    {}
+    try
+    {
+        tmp = dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+    }
+    catch(const std::bad_cast& e)
+    {}
+    try
+    {
+        tmp = dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+    }
+    catch(const std::bad_cast& e)
+    { }
+    
 }
 
 int main()
 {
-    Data me;
-    me.name = "me";
-    me.age = 27;
-    std::cout << "Name : " << me.name << " and age : " << me.age << std::endl;
-    uintptr_t test = serialize(&me);
-    Data *me2 = deserialize(test);
-    std::cout << "Name : " << me2->name << " and age : " << me2->age << std::endl;
+    srand(time(0));
+    Base *a[10];
+    for (int i = 0; i < 10; i++)
+        a[i] = generate();
+    std::cout << "identifty with ptr" << std::endl;
+    for (int i = 0; i < 10; i++)
+        identify(a[i]);
+    Base & ref0 = *a[9]; 
+    Base & ref1 = *a[8]; 
+    Base & ref2 = *a[7];
+    Base & ref3 = *a[6]; 
+    Base & ref4 = *a[5]; 
+    Base & ref5 = *a[4];
+    Base & ref6 = *a[3]; 
+    Base & ref7 = *a[2]; 
+    Base & ref8 = *a[1];
+    Base & ref9 = *a[0];
+    std::cout << "identifty with ref" << std::endl;
+    identify(ref0);
+    identify(ref1);
+    identify(ref2);
+    identify(ref3);
+    identify(ref4);
+    identify(ref5);
+    identify(ref6);
+    identify(ref7);
+    identify(ref8);
+    identify(ref9);
+
     return 0;
 }
