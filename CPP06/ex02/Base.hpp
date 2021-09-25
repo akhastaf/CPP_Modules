c2bede88acb7e0e7fc2e2c0fab7b3a126baf5b81
Abderrazzaq Khastaf <akhastaf@e0r3p2.1337.ma>
2021-09-25 12:55:38 +0100

    add ex02 to CPP06

---
+++ CPP06/ex02/Base.hpp
@@ -0,0 +1,13 @@
+#ifndef BASE_HPP
+# define BASE_HPP
+# include <iostream>
+
+class Base {
+	virtual ~Base(){};
+};
+
+class A : public Base {};
+class B : public Base {};
+class C : public Base {};
+
+#endif
